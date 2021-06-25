9eb46cc62566fbf691a68460f2fc9c3fd6392642
sheng31 <1445089856@qq.com>
2021-06-25 20:25:20 +0800

    添加项目文件。

---
+++ CommonV5/CommonHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonV5
+{
+    public class CommonHelper
+    {
+        public string Test()
+        {
+            return DateTime.Now.ToString("G");
+        }
+    }
+}
