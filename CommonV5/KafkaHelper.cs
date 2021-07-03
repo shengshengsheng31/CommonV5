49fa5d81b736e955e48b78604b7a3c335d67ca96
sheng31 <1445089856@qq.com>
2021-07-03 22:14:39 +0800

    kafka消费优化

--- CommonV5/KafkaHelper.cs
+++ CommonV5/KafkaHelper.cs
@@ -1,61 +1,79 @@
 ﻿using Confluent.Kafka;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CommonV5
 {
     public class KafkaHelper
     {
-        public static void KafkaConsumer(string bootstrapserver, string toppicName, string groupId,Action<string> MessageHandler)
+        /// <summary>
+        /// kafka消费
+        /// </summary>
+        /// <param name="bootstrapserver"></param>
+        /// <param name="toppicName"></param>
+        /// <param name="groupId"></param>
+        /// <param name="MessageHandler"></param>
+        /// <param name="cts"></param>
+        public static void KafkaConsumer(string bootstrapserver, string toppicName, string groupId, CancellationTokenSource cts, bool isAlwaysLatest,Action<string> MessageHandler)
         {
             // kafka配置
             ConsumerConfig conf = new ConsumerConfig
             {
                 GroupId = groupId,
                 BootstrapServers = bootstrapserver,
-                AutoOffsetReset = AutoOffsetReset.Latest,//初始状态读取最新数据
-                EnableAutoCommit = true,//自动提交偏移
-                EnableAutoOffsetStore = false,//不阻塞的commit
-                StatisticsIntervalMs = 5000,
-                SessionTimeoutMs = 6000,
-                EnablePartitionEof = true
+                AutoOffsetReset = AutoOffsetReset.Latest,   //初始状态读取最新数据
+                EnableAutoCommit = true,    //定期自动提交偏移
+                EnableAutoOffsetStore = false   //不阻塞的commit
             };
             // 监听
             using (IConsumer<Ignore, string> consumer = new ConsumerBuilder<Ignore, string>(conf).Build())
             {
                 consumer.Subscribe(toppicName);
-                int partitionCount = consumer.Assignment.Count;
-                for (int i = 0; i < partitionCount; i++)
+                while (consumer.Assignment.Count==0)
                 {
-                    // 从最新处开始消费
-                    TopicPartition topicPartition = new TopicPartition(toppicName, i);
-                    consumer.Seek(new TopicPartitionOffset(topicPartition, consumer.GetWatermarkOffsets(topicPartition).High));
-
+                    // 等待一会，获取partition数量
                 }
-
-                while (true)
+                // 始终从最新处开始消费
+                if (isAlwaysLatest)
                 {
-                    ConsumeResult<Ignore, string> consumeResult = consumer.Consume();
-                    if (consumeResult.Message!= null)
+                    for (int i = 0; i < consumer.Assignment.Count; i++)
                     {
-                        try
+                        TopicPartition topicPartition = new TopicPartition(toppicName, i);
+                        consumer.Seek(new TopicPartitionOffset(topicPartition, consumer.GetWatermarkOffsets(topicPartition).High));
+                    }
+                }
+                Log.Information($"topic:{toppicName} partitionCount:{consumer.Assignment.Count}");
+                try
+                {
+                    // 持续监听至cancel
+                    while (true)
+                    {
+                        ConsumeResult<Ignore, string> consumeResult = consumer.Consume(cts.Token);
+                        if (consumeResult.Message != null)
                         {
-                            Console.WriteLine($"partition：{consumeResult.Partition}-offset：{consumeResult.Offset}-range:{consumer.GetWatermarkOffsets(new TopicPartition(toppicName,consumeResult.Partition))}");
-                            MessageHandler(consumeResult.Message.Value);
-                        }
-                        catch (Exception ex)
-                        {
-
-                            MessageHandler(ex.Message);
-                        }
-                        finally
-                        {
-                            consumer.StoreOffset(consumeResult);
+                            try
+                            {
+                                //Console.WriteLine($"partition：{consumeResult.Partition} offset：{consumeResult.Offset} latest:{consumer.GetWatermarkOffsets(new TopicPartition(toppicName,consumeResult.Partition)).High}");
+                                MessageHandler(consumeResult.Message.Value);
+                            }
+                            finally
+                            {
+                                // 手动提交offset
+                                consumer.StoreOffset(consumeResult);
+                            }
                         }
                     }
+                }
+                // 检测到终止结束监听
+                catch (OperationCanceledException ex)
+                {
+                    Serilog.Log.Error(ex, ex.Message);
+                    consumer.Close();
                 }
             }
         }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CommonV5;
+using Serilog;
 using System;
 using System.Text.Json;
 using System.Threading;
@@ -9,34 +10,45 @@ namespace ConsoleApp
     class Program
     {
         static int taskCount = 0;
-        static  void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //ThreadTest().GetAwaiter().GetResult();
-            //Console.WriteLine("------------------1---------------");
-            //ThreadTest().GetAwaiter();
-            //Console.WriteLine("------------------2---------------");
+            // 日志记录，使用serilog接管全局日志
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File($"logs\\Console.log", rollingInterval: RollingInterval.Day)
+                .MinimumLevel.Debug()
+                .CreateLogger();
+            Log.Information("开始运行");
 
-            //string bootstrapserver = "49.234.93.236:9092";
-            //string toppicName = "test";
-            //string groupId = "sheng31";
-            string bootstrapserver = "32.96.1.69:9092";
-            string toppicName = "BAYONET_VEHICLEPASS_JSON_TOPIC";
+            Console.WriteLine("Hello World!");
+
+            string bootstrapserver = "49.234.93.236:9092";
+            string toppicName = "test";
             string groupId = "sheng31";
-            KafkaHelper.KafkaConsumer(bootstrapserver, toppicName, groupId, value=> {
+            //string bootstrapserver = "32.96.1.69:9092";
+            //string toppicName = "BAYONET_VEHICLEPASS_JSON_TOPIC";
+            //string groupId = "sheng31";
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            // 停止消费
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+            KafkaHelper.KafkaConsumer(bootstrapserver, toppicName, groupId, cts,true, value =>
+            {
                 try
                 {
                     VehicleDto.Root vehicle = JsonSerializer.Deserialize<VehicleDto.Root>(value);
-                    
                     string plateNo = vehicle.vehicleRcogResult[0].target[0].vehicle.plateNo.value;
                     string passTime = vehicle.vehicleRcogResult[0].targetAttrs.passTime;
                     string crossingName = vehicle.vehicleRcogResult[0].targetAttrs.cameraName;
                     Console.WriteLine($"passTime:{passTime}-plateNo:{plateNo}-crossingName{crossingName}");
                 }
-                catch (Exception ex)
+                // 解析失败
+                catch (Exception)
                 {
-                    Console.WriteLine(value.Substring(0,100));
-                    
+                    Console.WriteLine(value);
                 }
             });
             Console.ReadLine();
